a23245c241abdfbcb53245f46ef6430c0d794f25
Aleksander Petuskey <17petuskey@berkeley.edu>
2022-01-09 17:08:57 -0800

    Deploy Website

---
+++ build/precache-manifest.16a00e762266c6558955ffc399af2483.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b526cb68f3a1bc727b8418b8bfcf983",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "40823ff90736bae9242d",
+    "url": "/home/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "4c8256031dea4138a31e",
+    "url": "/home/static/js/2.e2792814.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/home/static/js/2.e2792814.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40823ff90736bae9242d",
+    "url": "/home/static/js/main.58bfc163.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "263eff8e16ffe2ed04344e86c1595503",
+    "url": "/home/static/media/alekpetuskey.263eff8e.jpeg"
+  },
+  {
+    "revision": "a96a14059fd131277265270567920cac",
+    "url": "/home/static/media/resume.a96a1405.pdf"
+  }
+]);
